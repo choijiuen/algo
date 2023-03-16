0e7f45007cd4b2ef41961b5794adfaf0f40730e9
choijiuen <choijiuen@gmail.com>
2023-03-16 22:43:21 +0900

    프로그래머스 42577번 전화번호 목록

---
+++ src/main/java/pro42577.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class pro42577 {
+    public static void main(String[] args) {
+
+    }
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        Arrays.sort(phone_book);
+
+        for(int i=0; i<phone_book.length-1; i++){
+            if(phone_book[i+1].startsWith(phone_book[i])){
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+}
+
+
