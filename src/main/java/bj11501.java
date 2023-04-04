75e06a2ef2ee78c443fc13b457e309801bc006e7
choijiuen <choijiuen@gmail.com>
2023-04-04 23:51:41 +0900

    백준 11501번 - 주식 - 1

---
+++ src/main/java/bj11501.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj11501 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<T; i++){
+            int N = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
+
+
+            }
+        }
+
+
+    }
+}
